7ae5c1b6d7b6e61b2562648ee00965bb7c5b9489
Hans Kruse <kruse@hanskruse.eu>
2024-07-12 17:52:18 +0200

    Upgraded keymap for testing

---
+++ config/custom_modifiers.h
@@ -0,0 +1,36 @@
+// custom modifier combinations
+#define LSG LS(LGUI)
+#define LSGK(key) LS(LG(key))
+
+#define RSG RS(RGUI)
+#define RSGK(key) RS(RG(key))
+
+#define LSC LS(LCTRL)
+#define LSCK(key) LS(LC(key))
+
+#define RSC RS(RCTRL)
+#define RSCK(key) RS(RC(key))
+
+#define LSA LS(LALT)
+#define LSAK(key) LS(LA(key))
+
+#define RSA RS(RALT)
+#define RSAK(key) RS(RA(key))
+
+#define LCA LC(LALT)
+#define LCAK(key) LC(LA(key))
+
+#define RCA RC(RALT)
+#define RCAK(key) RC(RA(key))
+
+#define LCG LC(LGUI)
+#define LCGK(key) LC(LG(key))
+
+#define RCG RC(RGUI)
+#define RCGK(key) RC(RG(key))
+
+#define LAG LA(LGUI)
+#define LAGK(key) LA(LG(key))
+
+#define RAG RA(RGUI)
+#define RAGK(key) RA(RG(key))
